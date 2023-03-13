4d7f003f6c21e38524e87c7fb9f2cde6345e05a2
Nikita <nik_zenkin@mail.ru>
2023-03-13 07:56:11 +0700

    Loading Count_order_system.c

---
+++ Count_order_system.c
@@ -0,0 +1,26 @@
+// Функция смены системы счисления с десятичной на указанную во втором аргументе ф-ии.
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define NUM 10
+
+int change (int a, int b){
+    int c = 0;
+    if(a / b == 0){
+        printf("%d", a % b);
+    }
+    else {
+        change (a / b, b);
+        printf("%d", a % b);
+    }
+}
+
+int main() {
+    int x = 0, y = 0;
+    scanf("%d %d", &x, &y);
+    change(x, y);
+    return 0;
+}
+// 10011101110101
+// 2333
